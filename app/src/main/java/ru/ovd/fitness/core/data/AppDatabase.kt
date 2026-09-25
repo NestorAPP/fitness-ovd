b463454a96ea5a3f64966aea164d990178b43dd4
NestorAPP <yura_nesteryonok@mail.ru>
2026-09-25 16:06:50 +0300

    Update AppDatabase.kt

--- app/src/main/java/ru/ovd/fitness/core/data/AppDatabase.kt
+++ app/src/main/java/ru/ovd/fitness/core/data/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -27,6 +28,7 @@ import ru.ovd.fitness.core.data.entity.QualificationScore
     version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun dao(): AppDao
@@ -50,9 +52,6 @@ abstract class AppDatabase : RoomDatabase() {
         }
     }
 
-    /**
-     * Заполняет базу данными при первом создании.
-     */
     private class DatabaseCallback(
         private val context: Context
     ) : RoomDatabase.Callback() {
